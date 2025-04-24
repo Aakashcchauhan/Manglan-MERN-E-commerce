8cf6af2a3ce5a49b2b38027d47eee4db26343308
Aakash Chuahan <130996799+Aakashcchauhan@users.noreply.github.com>
2025-04-24 23:54:31 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  sslValidate: false, // ← disables cert validation
+  useUnifiedTopology: true
 });
 
 
