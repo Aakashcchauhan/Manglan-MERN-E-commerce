49aa89b953faddb75750df13cdd032c83bce3225
Aakash Chuahan <130996799+Aakashcchauhan@users.noreply.github.com>
2025-04-25 23:47:31 +0530

    Update Collection.jsx

--- frontend/src/component/home/Collection.jsx
+++ frontend/src/component/home/Collection.jsx
@@ -135,7 +135,7 @@ export default function Collection() {
         </div>
       ) : (
         <div
-          className="bg-transparent flex justify-center items-center mt-10 overflow-hidden "
+          className="bg-transparent flex justify-center items-center mt-10  "
           style={{ height: `${containerHeight}px` }}
         >
           <div className="w-full md:w-[90%] mx-auto">
