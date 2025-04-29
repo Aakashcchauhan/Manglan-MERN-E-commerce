5d61ee36acf0a10653c6e93d96b470c2ee5cadd3
Aakash Chuahan <130996799+Aakashcchauhan@users.noreply.github.com>
2025-04-29 10:12:25 +0530

    Update banner.jsx

--- frontend/src/component/home/banner.jsx
+++ frontend/src/component/home/banner.jsx
@@ -7,7 +7,17 @@ const Banner = ({ images }) => {
     console.error("Banner component requires an array of at least 3 images");
     return <div className="w-full p-4">Banner images not available</div>;
   }
-
+const images = [
+{ 
+  image : "https://image.hm.com/content/dam/global_campaigns/season_01/men/start-page-assets/w17/MS21LH10-16x9-Startpage-Teaser-1-w17.jpg?imwidth=1660"
+},
+{
+  image : "https://image.hm.com/content/dam/global_campaigns/season_01/women/startpage-assets/wk17/WS21K-16x9-splash-women-startpage-wk17.jpg?imwidth=1920"
+},
+{
+  image: "https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-17/4081B-16x9-NS-kids-start-page-prio-week-17.jpg?imwidth=1660"
+}
+]
   return (
     <div className="w-full flex flex-col gap-4 p-4">
       {/* Container for all three images */}
@@ -15,7 +25,7 @@ const Banner = ({ images }) => {
         {/* Top banner */}
         <div className="w-full h-[50%] relative overflow-hidden bg-gray-100">
           <img 
-            src={images[0].url || "https://image.hm.com/content/dam/global_campaigns/season_01/men/start-page-assets/w17/MS21LH10-16x9-Startpage-Teaser-1-w17.jpg?imwidth=1660"}
+            src={images[0].url || images[0].image }
             alt="Main Banner" 
             className="w-full h-full object-cover"
           />
@@ -32,7 +42,7 @@ const Banner = ({ images }) => {
         <div className="w-full flex flex-col md:flex-row gap-4">
           <div className="w-full md:w-1/2 h-98 relative overflow-hidden bg-gray-100">
             <img 
-              src={images[1].url || "https://image.hm.com/content/dam/global_campaigns/season_01/women/startpage-assets/wk17/WS21K-16x9-splash-women-startpage-wk17.jpg?imwidth=1920"}
+              src={images[1].url || images[1].image}
               alt="Left Image" 
               className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" 
             />
@@ -50,7 +60,7 @@ const Banner = ({ images }) => {
           {/* Right image */}
           <div className="w-full md:w-1/2 h-98 relative overflow-hidden bg-gray-100">
             <img 
-              src={images[2].url || "https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-17/4081B-16x9-NS-kids-start-page-prio-week-17.jpg?imwidth=1660"}
+              src={images[2].url || images[2].image }
               alt="Right Image" 
               className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" 
             />
