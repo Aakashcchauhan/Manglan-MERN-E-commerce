e2da7691ec9e3327e8d9e2d0c048d721b67f86ce
Aakash Chuahan <130996799+Aakashcchauhan@users.noreply.github.com>
2025-04-29 10:26:15 +0530

    Update banner.jsx

--- frontend/src/component/home/banner.jsx
+++ frontend/src/component/home/banner.jsx
@@ -7,7 +7,7 @@ const Banner = ({ images }) => {
     console.error("Banner component requires an array of at least 3 images");
     return <div className="w-full p-4">Banner images not available</div>;
   }
-const images = [
+const BannerImage = [
 { 
   image : "https://image.hm.com/content/dam/global_campaigns/season_01/men/start-page-assets/w17/MS21LH10-16x9-Startpage-Teaser-1-w17.jpg?imwidth=1660"
 },
@@ -25,7 +25,7 @@ const images = [
         {/* Top banner */}
         <div className="w-full h-96 relative overflow-hidden bg-gray-100">
           <img 
-            src={images[0].url || images[0].image}
+            src={images[0].url || BannerImage[0].image}
             alt="Main Banner" 
             className="w-full h-full object-cover"
           />
@@ -42,7 +42,7 @@ const images = [
         <div className="w-full flex flex-col md:flex-row gap-4">
           <div className="w-full md:w-1/2 h-64 relative overflow-hidden bg-gray-100">
             <img 
-              src={images[1].url || images[1].image}
+              src={images[1].url || BannerImage[1].image}
               alt="Left Image" 
               className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" 
             />
@@ -60,7 +60,7 @@ const images = [
           {/* Right image */}
           <div className="w-full md:w-1/2 h-64 relative overflow-hidden bg-gray-100">
             <img 
-              src={images[2].url || images[2].image}
+              src={images[2].url || BannerImage[2].image}
               alt="Right Image" 
               className="w-full h-full object-cover hover:scale-105 transition-transform duration-300" 
             />
